5dff736fe858d27edc26f014edc812313478b1d3
ostudent826 <o.student826@gmail.com>
2024-12-18 21:57:44 +0200

    Added new version files

---
+++ App/web_app.py
@@ -0,0 +1,24 @@
+from flask import Flask ,render_template
+import os
+import signal
+
+app = Flask(__name__)
+
+@app.route('/get_user_name/<int:id>', methods=['GET'])
+def get_user_name(id):
+    # Render the frontend page with the provided user ID
+    # The page (frontend.html) will send backend requests to fetch and display user data
+    return render_template("frontend.html", id=id)
+
+@app.route('/stop_server')
+def stop_server():
+    print("rest_app - Stopped")
+    os._exit(0)
+    return 'Server stopped'
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5001,debug=True)
