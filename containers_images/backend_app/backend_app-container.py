efe1f695882141d602e65945528a5602ce2485d5
ostudent826 <o.student826@gmail.com>
2024-12-20 12:38:29 +0200

    commit

--- containers_images/backend_app/backend_app-container.py
+++ containers_images/backend_app/backend_app-container.py
@@ -232,4 +232,4 @@ def stop_server():
 
 if __name__ == '__main__':
     # Start the Flask application
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
--- containers_images/frontend_app/web_app-container.py
+++ containers_images/frontend_app/web_app-container.py
@@ -21,4 +21,4 @@ def stop_server():
 
 
 if __name__ == '__main__':
-    app.run(port=5001,debug=True)
+    app.run(host='0.0.0.0',port=5001,debug=True)
